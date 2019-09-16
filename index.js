8afa7307c1cc3853e0db320c09d111bae5fc02b2
RCI\sagar.gandhi <sagar.gandhi@rci.rogers.com>
2019-09-16 11:52:23 -0400

    sagar adding getter methods

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const {
   poweredByHandler
 } = require('./handlers.js')
 
+var HEIGHT;
+var WIDTH;
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
@@ -24,7 +26,9 @@ app.use(poweredByHandler)
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
   // NOTE: Do something here to start the game
-
+  var gameObject = request.body;
+  HEIGHT = gameObject.board.height;
+  WIDTH = gameObject.board.width;
   // Response data
   const data = {
     color: '#ff5354',
@@ -35,10 +39,26 @@ app.post('/start', (request, response) => {
   return response.json(data)
 })
 
+function getSnakes(gameObject)
+{
+    return gameObject.board.snakes;
+}
+
+function getFood(gameObject)
+{
+    return gameObject.board.food;
+}
+
+function getCurrentLocation(gameObject)
+{
+    return gameObject.you.body;
+}
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
   console.log(request); // Check heroku logs
+
+
 
   // Response data
   const data = {
